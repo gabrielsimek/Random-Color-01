dbe5fe2e6afc0f6c29d62747399d801eb6379bde
Gabriel Simek <gabrielsimek@outlook.com>
2021-07-26 22:49:35 -0700

    stretch working

--- src/components/random-color/RandomColorDisplay.jsx
+++ src/components/random-color/RandomColorDisplay.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 function RandomColorDisplay({ backgroundColor }) {
+  let style;
+  if(backgroundColor.length < 10){
+    style = {
+      backgroundColor,
+      width: '100vw',
+      height: '100vh'
+    };
+  }
+  else {
+    style = {
+      backgroundImage: backgroundColor,
+      width: '100vw',
+      height: '100vh'
+    };
+  }
+
+  
+  
+
   return (
     <div
-      style= {{
-        backgroundColor,
-        width: '100vw',
-        height: '100vh'
-      }}
+      style={style}
     >        
     </div>
   );
 }
 RandomColorDisplay.propTypes = {
   backgroundColor: PropTypes.string.isRequired,
+  backgroundImage: PropTypes.string
 };
 
 export default RandomColorDisplay;
--- src/containers/RandomColor.js
+++ src/containers/RandomColor.js
@@ -10,12 +10,20 @@ export default class RandomColor extends Component {
     setInterval(() => {
       const randomNumber = Math.floor(Math.random() * colors.length);
       this.setState({ selectedColor: colors[randomNumber] });
+      console.log(colors[randomNumber]);
     }, 1000);
   }
-  render() {
-    return (
-      <RandomColorDisplay backgroundColor={this.state.selectedColor}/>
-    );
-  }
+    componentDidUpdate = (prevProp, prevState) => {
+      if(prevState.selectedColor === this.state.selectedColor){
+        console.log('samesies');
+        this.setState({ selectedColor: 'url(\'https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg\')' });
+      }
+
+    }
+    render() {
+      return (
+        <RandomColorDisplay backgroundColor={this.state.selectedColor}/>
+      );
+    }
 
 }
